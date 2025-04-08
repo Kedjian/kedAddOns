89fea32f0e1b0c5656eabdeaa175e1e8aca6f818
Kedjian <o.mat.makowski@gmail.com>
2025-04-09 01:58:20 +0200

    initial commit

---
+++ src/main/java/net/ked/tutorialmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.ked.tutorialmod.item;
+
+import net.ked.tutorialmod.KedMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, KedMod.MOD_ID);
+
+    public static final RegistryObject<Item> KEDANDRYT = ITEMS.register("kedandryt",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> REFANDRYT = ITEMS.register("refandryt",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> DADOSZANDRYT = ITEMS.register("dadoszandryt",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
